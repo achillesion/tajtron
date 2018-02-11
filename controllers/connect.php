c503be00b012ef218536c3a4a7fb2c97ca4586ac
achillesion <achillesion@gmail.com>
2018-02-12 00:10:54 +0500

    Remote Database added

--- controllers/connect.php
+++ controllers/connect.php
@@ -2,12 +2,12 @@
 
 // Coonection | User | Pasword | Database
 
-$db_host        = 'https://premium34.web-hosting.com';
-$db_user        = 'tajtron';
-$db_pass        = 'jaytron69';
-$db_database    = 'looptelp_tajtron';
+$db_host        = 'sql12.freemysqlhosting.net';
+$db_user        = 'sql12220763';
+$db_pass        = 'ARjQpgyv5a';
+$db_database    = 'sql12220763';
 $db_port        = '3306';
-$con = mysqli_connect("localhost",$db_user ,$db_pass ,$db_database );
+$con = mysqli_connect($db_host,$db_user ,$db_pass ,$db_database );
 
 // Check connection
 if (mysqli_connect_errno())
